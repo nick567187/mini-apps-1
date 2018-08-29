d919920ed5f5376406443609efaa56e5099bd1e1
Hack Reactor Student <student@hackreactor.com>
2018-08-28 21:42:45 -0700

    last changes

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,37 @@
+$('document').ready(function() {
+  $('#submitButton').click(function(event) {
+    event.preventDefault();
+    $('#cv').empty();
+    var data = $('#textBox').val();
+    $httpPost(data);
+    $('#textBox').val('');
+  });
+});
 
+var view = {
+  renderCV: function(array) {
+    var $filter = $('#filter').val();
+    $filter.toLowerCase();
+    if($filter !== '') {
+      array.filter((elem) => {
+        return !elem.toLowerCase().includes($filter);
+      }).forEach((word) => {
+        $('#cv').append(`<p>${word}</p>`);
+        $('#cv').append('<br>');
+      });
+    } else {
+      array.forEach((elem) => {
+        $('#cv').append(`<p>${elem}</p>`);
+        $('#cv').append('<br>');
+      });
+    }
+  }
+}
 
+var $httpPost = function(data) {
+  data = {result: data};
+  $.post('/', data, function(result) {
+    var results = result.split('\n');
+    view.renderCV(results);
+  });
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,49 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const fs = require('fs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('client'));
 
+app.post('/', (req,res) => {
+  var result = JSON.parse(req.body.result);
+  var result2 = generateCV(result);
+  fs.writeFile('./samples/csv_report.csv', result2, (err) => {
+    if(err) { console.log(err) };
+    res.send(result2);
+  });
+});
+
+var generateCV = function(data) {
+  var firstLine = [];
+  var results;
+  for(var key in data) { 
+    if(key !== 'children') {
+      firstLine.push(key); 
+    }
+  };
+  firstLine = firstLine.join(',');
+  results = firstLine;
+
+  var recursiveHelper = function(next) {
+    var curr = next;
+    var nextLine = [];
+    for(var key in curr) {
+      if(key !== 'children') {
+        nextLine.push(curr[key]);
+      }
+    }
+    firstLine = firstLine.concat('\n', nextLine);
+    results = results.concat('\n', nextLine);
+    for(var i = 0; i < curr.children.length; i++) {
+      recursiveHelper(curr.children[i]);
+    }
+  }
+
+  recursiveHelper(data);
+  return results;
+}
+
+app.listen(3000, () => console.log('I am listening on port 3000'));
